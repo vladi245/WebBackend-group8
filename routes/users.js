e3204b4b01e746b61ffbd908a8c67af615ad00d9
V3ktorr <vbarabas390gmail.com>
2025-11-19 22:25:52 +0100

    i understand it now

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getAllUsers } = require('../models/User');
+const { getAllUsers, UserModel } = require('../models/User');
 
 // Route to return only the user's name
 router.get('/name', (req, res) => {
@@ -17,4 +17,17 @@ router.get('/users', async (req, res) => {
   }
 });
 
+
+router.delete('/users/id', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    await UserModel.deleteById(userId);
+    res.json({ message: "User deleted" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Couldn't delete user" });
+  }
+});
+
 module.exports = router;
+
