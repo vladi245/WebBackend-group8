0062f576c44c497379457c57e75c440d73c93a47
0xZarok <vlad.ionita.vi@gmail.com>
2025-11-22 15:15:52 +0100

    1

---
+++ routes/workouts.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import authMiddleware from '../src/middleware/authMiddleware.js';
+import { workoutsController } from '../controllers/workoutsController.js';
+
+const router = express.Router();
+
+// Protected endpoints for user's workout records and stats
+router.post('/', authMiddleware, workoutsController.addCompletedWorkout);
+router.get('/', authMiddleware, workoutsController.getUserWorkouts);
+router.get('/stats', authMiddleware, workoutsController.getUserStats);
+router.delete('/:id', authMiddleware, workoutsController.deleteRecord);
+
+export default router;
