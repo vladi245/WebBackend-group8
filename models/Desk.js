3d6c28691056bd66d323e690afc9c0f1a8f86df2
0xZarok <vlad.ionita.vi@gmail.com>
2025-11-29 17:43:13 +0100

    added create desk

---
+++ models/Desk.js
@@ -0,0 +1,33 @@
+const pool = require('../src/db.js');
+
+async function getAllDesks() {
+    const res = await pool.query('SELECT * FROM desk ORDER BY id ASC');
+    return res.rows;
+}
+
+async function getDeskById(id) {
+    const res = await pool.query('SELECT * FROM desk WHERE id = $1', [id]);
+    return res.rows[0] || null;
+}
+
+async function createDesk({ id, height }) {
+    if (id === undefined) {
+        const res = await pool.query('INSERT INTO desk (height) VALUES ($1) RETURNING *', [height]);
+        return res.rows[0] || null;
+    }
+
+    const res = await pool.query('INSERT INTO desk (id, height) VALUES ($1, $2) RETURNING *', [id, height]);
+    return res.rows[0] || null;
+}
+
+async function updateDeskHeight(id, height) {
+    const res = await pool.query('UPDATE desk SET height = $2 WHERE id = $1 RETURNING *', [id, height]);
+    return res.rows[0] || null;
+}
+
+module.exports = {
+    getAllDesks,
+    getDeskById,
+    createDesk,
+    updateDeskHeight,
+};
